11f3ec6757d2687616ef00f6a09038dfece845dc
geun25 <sas0898@naver.com>
2021-12-18 20:43:36 +0900

    .

---
+++ PandaCoding/Struct/Struct.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	struct Person
+	{
+		string name;
+		string position;
+		int height;
+		int weight;
+	};
+
+	Person p = { "Son", "striker", 183, 77 };
+	cout << p.name << endl;
+
+	Person a[2] = {
+		{"A", "A", 1, 1},
+		{"B", "B", 2, 2}
+	};
+
+	cout << a[1].height << endl;
+
+	return 0;
+}
+
---
+++ PandaCoding/cincout/cincout.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	const int Size = 15;
+	char name1[Size];
+	char name2[Size] = "C++programming";
+
+	cout << "이름이 뭐에요?\n";
+	cin.getline(name1, Size);
+	cout << strlen(name1) << endl;
+	cout << sizeof(name1) << endl;
+	cout << name2 << endl;
+}
